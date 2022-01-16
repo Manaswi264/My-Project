7e105cd231ad086f91304041cfe9e331d61994d3
Manaswi264 <90857044+Manaswi264@users.noreply.github.com>
2022-01-16 07:21:46 +0530

    Add files via upload

---
+++ classesAndObjects.java
@@ -0,0 +1,43 @@
+
+public class classesAndObjects {
+	
+	    String name; 
+	    String breed; 
+	    int age; 
+	    String color; 
+	    public classesAndObjects(String name, String breed, int age, String color) 
+	    { 
+	        this.name = name; 
+	        this.breed = breed; 
+	        this.age = age; 
+	        this.color = color; 
+	    } 
+	    public String getName() 
+	    { 
+	        return name; 
+	    } 
+	    public String getBreed() 
+	    { 
+	        return breed; 
+	    } 
+	    public int getAge() 
+	    { 
+	        return age; 
+	    } 
+	    public String getColor() 
+	    { 
+	        return color; 
+	    } 
+	    @Override
+	    public String toString() 
+	    { 
+	        return("Hi my name is "+ this.getName()+ ".\nMy breed,age and color are " + this.getBreed()+", " + this.getAge()+ ", and"+ this.getColor() + "."); 
+	    } 
+	    public static void main(String[] args) 
+	    { 
+	       classesAndObjects scott = new classesAndObjects("Scott","papillon", 5, "black"); 
+	        System.out.println(scott.toString()); 
+	    } 
+	}
+
+
